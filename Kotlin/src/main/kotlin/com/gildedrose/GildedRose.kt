5b332cf98f42b162be5b5e01f2316a62c7aec6f5
Clara BG <clara.bollengandolfo@gmail.com>
2025-07-04 09:32:07 +0100

    attempting to create PR

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,7 +2,7 @@ package com.gildedrose
 
 class GildedRose(val items: List<Item>) {
 
-    // 
+    //
     fun updateQuality() {
         for (i in items.indices) {
             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
@@ -16,17 +16,7 @@ class GildedRose(val items: List<Item>) {
                 }
             }
 
-            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].sellIn < 0) {
-                    items[i].quality = 0
-                } else if (items[i].sellIn in 6..10) {
-                    items[i].quality += 2
-                } else if (items[i].sellIn in 0..5) {
-                    items[i].quality += 3
-                } else {
-                    items[i].quality += 1
-                }
-            }
+           
 
             if (items[i].name == "+5 Dexterity Vest" || items[i].name == "Elixir og the Mongoose") {
 
